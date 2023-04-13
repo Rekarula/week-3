97963774a844ab56a51d675ebb82e001dd6e77e5
Rekarula <123720086+Rekarula@users.noreply.github.com>
2023-04-13 19:11:53 +0300

    add modal window

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,11 @@
-//elect movie poster original image
+//select movie poster original image
 let moviePoster = document.querySelector(".movie-poster");
 //select modal window layer
 let modal = document.querySelector("#myModal");
 //select close button
-let closeBtn = document.querySelector("closeBtn");
+let closeBtn = document.querySelector(".closeBtn");
 //select modal window image
-let modalImage = document.querySelector("movie-poster-modal");
+let modalImage = document.querySelector(".movie-poster-modal");
 
 
 moviePoster.addEventListener("click", ()=>{
